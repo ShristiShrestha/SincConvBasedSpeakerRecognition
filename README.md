b88405c2c3aee733345da84fd27d19d95e5e50c7
Shristi Shrestha <sthashristi777@gmail.com>
2019-06-18 14:30:40 +0545

    flask api for testing added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SincConvBasedSpeakerRecognition
+
+### for api:
+#### Run command: 
+> pip install flask
+> py -m venv <venv_name>
+> cd <venv_name>/scripts
+> activate
+>cd ../..
+> set FLASK_APP = api  #api.py run 
+> set FLASK_ENVIRONMENT= development  
+> flask run # run server at 127.0.0.1:5000
+
---
+++ app/api.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, jsonify
+#import requests
+import os, json
+import datetime
+
+app = Flask(__name__)
+app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template('./upload.html')
+
+@app.route('/identification', methods=['POST', 'GET'])
+def success():
+	if request.method == 'POST':
+		t=str(datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S'))
+		path="uploads/"
+		f = request.files['file']
+		#if not os.path.exists(path):
+		#	os.mkdir(path)
+		
+		#call validation function
+		speaker_id=0
+		username=""
+		total_time=""
+
+		f.save(os.path.join(path, t+".wav"))
+		res = jsonify(
+        value="file saved in "+os.path.join(path, t+".wav"),
+        status=200,
+        mimetype='application/json'
+    	)
+		return res
+	else:
+		return render_template('./upload.html')
+
+	
+'''
+@app.route('/forward', methods=['POST'])
+def forward():
+    print("Posted file: {}".format(request.files['audio']))
+    file = request.files['audio']
+    files = {'file': file.read()}
+    r = requests.post("http://127.0.0.1:8000/handle/", files=files) #end point for the next server
+
+    if r.ok:
+        return "File uploaded!"
+    else:
+        return "Error uploading file!"
+
+'''
+
+if __name__=='__main__':
+	app.run(debug=True)
